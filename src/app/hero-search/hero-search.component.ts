54d0522490a6efefae7aeeaf2638870cda68cd27
Gian Michel <gianmnf@unipam.edu.br>
2023-05-14 16:12:03 -0300

    Finishing project

---
+++ src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+import { Observable, Subject } from 'rxjs';
+
+import {
+   debounceTime, distinctUntilChanged, switchMap
+ } from 'rxjs/operators';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.css']
+})
+export class HeroSearchComponent {
+  heroes$!: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private heroService: HeroService) {}
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      // wait 300ms for keystrokes
+      debounceTime(300),
+      // Ignore if is the same term
+      distinctUntilChanged(),
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+    )
+  }
+}
